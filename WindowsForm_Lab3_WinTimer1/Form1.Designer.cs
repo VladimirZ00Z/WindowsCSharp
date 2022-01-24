3fcf396e6f171ae1d185e6b250b96ac59d960217
VladimirZ00Z <94064126+VladimirZ00Z@users.noreply.github.com>
2022-01-24 18:11:31 +0300

    Add files via upload

---
+++ WindowsForm_Lab3_WinTimer1/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControlTimer1 = new Lab3.UserControlTimer();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.userControlTime21 = new WinTimer2.UserControlTime2();
+            this.SuspendLayout();
+            // 
+            // userControlTimer1
+            // 
+            this.userControlTimer1.Location = new System.Drawing.Point(12, 73);
+            this.userControlTimer1.Name = "userControlTimer1";
+            this.userControlTimer1.Size = new System.Drawing.Size(51, 17);
+            this.userControlTimer1.TabIndex = 0;
+            this.userControlTimer1.TimeEnabled = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(112, 73);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(127, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Остановка таймера";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // userControlTime21
+            // 
+            this.userControlTime21.Location = new System.Drawing.Point(92, 12);
+            this.userControlTime21.Name = "userControlTime21";
+            this.userControlTime21.Size = new System.Drawing.Size(51, 17);
+            this.userControlTime21.TabIndex = 2;
+            this.userControlTime21.Text = "userControlTime21";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(251, 165);
+            this.Controls.Add(this.userControlTime21);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.userControlTimer1);
+            this.Name = "Form1";
+            this.Text = "Таймер";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private UserControlTimer userControlTimer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private WinTimer2.UserControlTime2 userControlTime21;
+    }
+}
+
---
+++ WindowsForm_Lab3_WinTimer1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked ==  false)
+            {
+                userControlTimer1.TimeEnabled = true;                
+            }
+            else
+            {
+                userControlTimer1.TimeEnabled = false;
+            }
+        }
+    }
+}
