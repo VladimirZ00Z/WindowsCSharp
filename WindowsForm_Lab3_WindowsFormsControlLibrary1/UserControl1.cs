afffde28dff22896e31b016b62b8ed11d1782d2e
VladimirZ00Z <94064126+VladimirZ00Z@users.noreply.github.com>
2022-01-24 18:11:31 +0300

    Add files via upload

---
+++ WindowsForm_Lab3_WindowsFormsControlLibrary1/UserControl1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsControlLibrary1
+{
+    public partial class UserControl1: UserControl
+    {
+        public bool TimeEnabled
+        {
+            get { return timer1.Enabled; }
+            set { timer1.Enabled = value; }
+        }
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
